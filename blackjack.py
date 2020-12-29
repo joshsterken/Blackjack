ae74f628fca4a299e5b0c693a3a5c393e2c363d1
Josh <joshsterken@gmail.com>
2020-12-29 00:08:21 -0600

    end of day update

--- blackjack.py
+++ blackjack.py
@@ -3,8 +3,8 @@ import os
 
 #card class
 class Card():
-    suits = ['Clubs', 'Hearts', 'Spades', 'Diamonds']
-    ranks = ['A', 'K', 'Q', 'J', '9', '8', '7', '6', '5', '4', '3', '2']
+    suits = ('Clubs', 'Hearts', 'Spades', 'Diamonds')
+    ranks = ('A', 'K', 'Q', 'J', '9', '8', '7', '6', '5', '4', '3', '2')
     values = {'A': 11, 'K': 10, 'Q': 10, 'J': 10, '9': 9, '8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3, '2': 2}
 
     def __init__(self, suit, rank, value):
